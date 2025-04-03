05fef74b79d5cd9206bcd8b8238baf99ae0dd597
Arindam Bose <adambose1990@gmail.com>
2025-04-02 23:18:27 -0400

    3d fft

--- src/cufft3d.cu
+++ src/cufft3d.cu
@@ -1,75 +1,94 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <cuda_runtime.h>
 #include <cufft.h>
 
-#define NX 64  // X-dimension size
-#define NY 64  // Y-dimension size
-#define NZ 64  // Z-dimension size
-#define ELEM_SIZE NX * NY * NZ
+#define PRINT_FLAG 0
+#define NPRINTS 30  // print size
+#define IFFT_FLAG 0
 
-void run_test_cufft_3d(int argc, char** argv) {
+void run_test_cufft_3d(unsigned int nx, unsigned int ny, unsigned int nz) {
+    srand(2025);
+
     // Declaration
-    cufftComplex *complex_samples, *new_complex_samples;
+    cufftComplex *complex_samples;
+    cufftComplex *new_complex_samples;
     cufftComplex *complex_freq;
     cufftComplex *d_complex_samples;
     cufftComplex *d_complex_freq;
     cufftHandle plan;
-    srand(2025);
+
+    unsigned int element_size = nx * ny * nz;
 
     // Allocate memory for the variables on the host
-    complex_samples = (cufftComplex *)malloc(sizeof(cufftComplex) * ELEM_SIZE);
-    complex_freq = (cufftComplex *)malloc(sizeof(cufftComplex) * ELEM_SIZE);
-    new_complex_samples = (cufftComplex *)malloc(sizeof(cufftComplex) * ELEM_SIZE);
+    complex_samples = (cufftComplex *)malloc(sizeof(cufftComplex) * element_size);
+    complex_freq = (cufftComplex *)malloc(sizeof(cufftComplex) * element_size);
+    if (IFFT_FLAG) {new_complex_samples = (cufftComplex *)malloc(sizeof(cufftComplex) * element_size);}
 
-    for (unsigned int i = 0; i < ELEM_SIZE; i++) {
+    // Initialize input complex signal
+    for (unsigned int i = 0; i < element_size; i++) {
         complex_samples[i].x = rand() / (float)RAND_MAX;
         complex_samples[i].y = 0;
     }
 
     // Allocate device memory for complex signal and output frequency
-    cudaMalloc((void **)&d_complex_samples, sizeof(cufftComplex) * ELEM_SIZE);
-    cudaMalloc((void **)&d_complex_freq, sizeof(cufftComplex) * ELEM_SIZE);
+    cudaMalloc((void **)&d_complex_samples, sizeof(cufftComplex) * element_size);
+    cudaMalloc((void **)&d_complex_freq, sizeof(cufftComplex) * element_size);
 
     // Copy host memory to device
-    cudaMemcpy(d_complex_samples, complex_samples, sizeof(cufftComplex) * ELEM_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_complex_samples, complex_samples, sizeof(cufftComplex) * element_size, cudaMemcpyHostToDevice);
 
     // Setup a 3D FFT plan
-    cufftPlan3d(&plan, NX, NY, NZ, CUFFT_C2C);
+    cufftPlan3d(&plan, nx, ny, nz, CUFFT_C2C);
 
     // Execute the forward 3D FFT (in-place computation)
     cufftExecC2C(plan, d_complex_samples, d_complex_freq, CUFFT_FORWARD);
 
     // Retrieve the results into host memory
-    cudaMemcpy(complex_freq, d_complex_freq, sizeof(cufftComplex) * ELEM_SIZE, cudaMemcpyDeviceToHost);
+    cudaMemcpy(complex_freq, d_complex_freq, sizeof(cufftComplex) * element_size, cudaMemcpyDeviceToHost);
 
-    // Execute the inverse 3D FFT (in-place computation)
-    cufftExecC2C(plan, d_complex_freq, d_complex_samples, CUFFT_INVERSE);
+    if (IFFT_FLAG) {
+        // Execute the inverse 3D IFFT (in-place computation)
+        cufftExecC2C(plan, d_complex_freq, d_complex_samples, CUFFT_INVERSE);
 
-    // Retrieve the results into host memory
-    cudaMemcpy(new_complex_samples, d_complex_samples, sizeof(cufftComplex) * ELEM_SIZE, cudaMemcpyDeviceToHost);
+        // Retrieve the results into host memory
+        cudaMemcpy(new_complex_samples, d_complex_samples, sizeof(cufftComplex) * element_size, cudaMemcpyDeviceToHost);
 
-    cudaDeviceSynchronize();
+        cudaDeviceSynchronize();
 
-    for (unsigned int i = 0; i < ELEM_SIZE; i++) {
-        new_complex_samples[i].x /= (float)ELEM_SIZE;
-        new_complex_samples[i].y /= (float)ELEM_SIZE;
+        // Normalize
+        for (unsigned int i = 0; i < element_size; i++) {
+            new_complex_samples[i].x /= (float)element_size;
+            new_complex_samples[i].y /= (float)element_size;
+        }
     }
 
-    for (unsigned int i = 0; i < 30; i++) {
-        printf("  %2.4f + i%2.4f -> %2.4f + i%2.4f\n", complex_samples[i].x, complex_samples[i].y, new_complex_samples[i].x, new_complex_samples[i].y);
+    if (PRINT_FLAG && IFFT_FLAG) {
+        printf("Complex samples after FFT and IFFT...\n");
+        for (unsigned int i = 0; i < NPRINTS; i++) {
+            printf("  %2.4f + i%2.4f -> %2.4f + i%2.4f\n", complex_samples[i].x, complex_samples[i].y, new_complex_samples[i].x, new_complex_samples[i].y);
+        }
     }
 
     // Clean up
     cufftDestroy(plan);
     cudaFree(d_complex_freq);
     cudaFree(d_complex_samples);
-    free(new_complex_samples);
+    if (IFFT_FLAG) {free(new_complex_samples);}
     free(complex_freq);
     free(complex_samples);
 }
 
 
 int main(int argc, char **argv) {
-    run_test_cufft_3d(argc, argv);
+    if (argc != 4) {
+        printf("Error: This program requires exactly 3 command-line arguments.\n");
+        return 1;
+    }
+
+    int nx = atoi(argv[1]);
+    int ny = atoi(argv[2]);
+    int nz = atoi(argv[3]);
+    run_test_cufft_3d(nx, ny, nz);
     return 0;
 }
---
+++ src/fft3d.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fftw3.h>
+
+#define PRINT_FLAG 0
+#define NPRINTS 30  // print size
+#define IFFT_FLAG 0
+
+void run_test_fft_3d(unsigned int nx, unsigned int ny, unsigned int nz) {
+    srand(2025);
+
+    // Declaration
+    fftw_complex *complex_samples;
+    fftw_complex *new_complex_samples;
+    fftw_complex *complex_freq;
+    fftw_plan plan_fft, plan_ifft;
+    
+    unsigned int element_size = nx * ny * nz;
+
+    // Allocate memory for input and output arrays
+    complex_samples = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * element_size);
+    complex_freq = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * element_size);
+    if (IFFT_FLAG) {new_complex_samples = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * element_size);}
+
+    // Initialize input complex signal
+    for (unsigned int i = 0; i < element_size; i++) {
+        complex_samples[i][0] = rand() / (float)RAND_MAX;
+        complex_samples[i][1] = 0;
+    }
+
+    // Setup the FFT plan
+    plan_fft = fftw_plan_dft_3d(nx, ny, nz, complex_samples, complex_freq, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    // Execute a complex-to-complex 3D FFT
+    fftw_execute(plan_fft);
+
+    if (IFFT_FLAG) {
+        // Setup the IFFT plan
+        plan_ifft = fftw_plan_dft_3d(nx, ny, nz, complex_freq, new_complex_samples, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+        // Execute a complex-to-complex 3D IFFT
+        fftw_execute(plan_ifft);
+
+        // Normalize
+        for (unsigned int i = 0; i < element_size; i++) {
+            new_complex_samples[i][0] /= (float)element_size;
+            new_complex_samples[i][1] /= (float)element_size;
+        }
+    }
+
+    // Print output stuff
+    if (PRINT_FLAG && IFFT_FLAG) {
+        printf("Complex samples after FFT and IFFT...\n");
+        for (unsigned int i = 0; i < NPRINTS; i++) {
+            if (IFFT_FLAG)
+            printf("  %2.4f + i%2.4f -> %2.4f + i%2.4f\n", complex_samples[i][0], complex_samples[i][1], new_complex_samples[i][0], new_complex_samples[i][1]);
+        }
+    }
+
+    // Cleanups
+    fftw_destroy_plan(plan_fft);
+    fftw_destroy_plan(plan_ifft);
+    if (IFFT_FLAG) {fftw_free(new_complex_samples);}
+    fftw_free(complex_samples);
+    fftw_free(complex_freq);
+}
+
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        printf("Error: This program requires exactly 3 command-line arguments.\n");
+        return 1;
+    }
+
+    int nx = atoi(argv[1]);
+    int ny = atoi(argv[2]);
+    int nz = atoi(argv[3]);
+    run_test_fft_3d(nx, ny, nz);
+    return 0;
+}
